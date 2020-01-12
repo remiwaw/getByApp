c72ef00076b499c13f425bef53be90092e6905c9
Remigiusz Wawrzyniak <remigiusz.wawrzyniak@ext.sys.aok.de>
2020-01-12 20:18:30 +0100

    Added carousel library with an mock view

--- app/src/main/java/com/rwawrzyniak/getby/OnboardingFragment.kt
+++ app/src/main/java/com/rwawrzyniak/getby/OnboardingFragment.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.rwawrzyniak.getby.databinding.FragmentOnboardingBinding
+import com.synnapps.carouselview.ImageListener
+import kotlinx.android.synthetic.main.fragment_tour.*
 
 
 class OnboardingFragment : Fragment() {
+
+    private var sampleImages = intArrayOf(
+        R.drawable.onboarding_one,
+        R.drawable.onboarding_two,
+        R.drawable.onboarding_three
+    )
 
     private lateinit var binding: FragmentOnboardingBinding
 
@@ -17,6 +26,11 @@ class OnboardingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+
+        binding.carouselView.pageCount = sampleImages.size
+        binding.carouselView.setImageListener { position, imageView ->
+            imageView.setImageResource(sampleImages[position])
+        }
 
         return binding.root
     }
