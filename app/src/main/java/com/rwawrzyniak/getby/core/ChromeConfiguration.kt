02f742b9612b036df65b2ca1ac39a2c53ecb4fe5
Remigiusz Wawrzyniak <remigiusz.wawrzyniak@ext.sys.aok.de>
2020-02-15 00:08:26 +0100

    Added bottom navigation

---
+++ app/src/main/java/com/rwawrzyniak/getby/core/ChromeConfiguration.kt
@@ -0,0 +1,6 @@
+data class ChromeConfiguration(
+    val showActionBar: Boolean = false,
+    val showActionBarUpButton: Boolean = true,
+    val actionBarTitle: String? = null,
+    val showBottomNavigationBar: Boolean = false
+)
---
+++ app/src/main/java/com/rwawrzyniak/getby/habits/HabitsFragment.kt
@@ -0,0 +1,7 @@
+package com.rwawrzyniak.getby.habits
+
+import androidx.fragment.app.Fragment
+
+class HabitsFragment : Fragment() {
+
+}
