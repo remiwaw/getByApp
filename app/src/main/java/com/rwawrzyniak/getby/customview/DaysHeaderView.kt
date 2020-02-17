8df35cb3a00adea7b557b4d1774fe7ec793e7ba5
Remigiusz Wawrzyniak <remigiusz.wawrzyniak@ext.sys.aok.de>
2020-02-17 21:11:55 +0100

    Dynamic populating days header

--- app/src/main/java/com/rwawrzyniak/getby/customview/DaysHeaderView.kt
+++ app/src/main/java/com/rwawrzyniak/getby/customview/DaysHeaderView.kt
@@ -3,12 +3,13 @@ package com.rwawrzyniak.getby.customview
 import addDays
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.children
 import asCalenderddMMyyy
 import com.rwawrzyniak.getby.R
 import kotlinx.android.synthetic.main.day_header_element.view.*
 import kotlinx.android.synthetic.main.days_list.view.*
-import nextDay
 import toDayHeaderDto
 
 class DaysHeaderView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
@@ -20,19 +21,13 @@ class DaysHeaderView(context: Context, attrs: AttributeSet) : ConstraintLayout(c
 
         val firstDayAsString = attributes.getString(R.styleable.DaysHeaderView_firstDayAsString)
 
-        val firstDayAsCalandar = firstDayAsString.asCalenderddMMyyy
+        val firstDayAsCalendar = firstDayAsString.asCalenderddMMyyy
 
-        listOf(
-            firstDayAsCalandar,
-            firstDayAsCalandar.nextDay,
-            firstDayAsCalandar.addDays(2),
-            firstDayAsCalandar.addDays(3)
-        )
-            .map { it.toDayHeaderDto }
-            .forEach {
-                label_a.dayNameView.text = it.shortName
-                label_a.dayNumberView.text = it.number.toString()
-            }
+        days_list_layout.children.forEachIndexed { index, item: View ->
+            val currentDay = firstDayAsCalendar.addDays(index).toDayHeaderDto
+            item.dayNameView.text = currentDay.shortName
+            item.dayNumberView.text = currentDay.number.toString()
+        }
 
         attributes.recycle()
     }
